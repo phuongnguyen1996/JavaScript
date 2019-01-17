fe308f8907eec0c0a3159abf35152f22a9f9eb06
phuongnguyen1996 <46129227+phuongnguyen1996@users.noreply.github.com>
2019-01-17 16:29:02 +0700

    Update async_2.js

--- async_2.js
+++ async_2.js
@@ -3,18 +3,18 @@
 //   console.log('Hello Fresher Academy')
 // }, 1000)
 
-// // Exercise 02 setInterval
-// var count = 0
-// var abc = setInterval(function () {
-//   count += 1
-//   stopInterval(count)
-//   console.log('Hello Fresher Academy')
-// }, 3000)
-// function stopInterval (count) {
-//   if (count > 2) {
-//     clearInterval(abc)
-//   }
-// }
+// Exercise 02 setInterval
+var count = 0
+var abc = setInterval(function () {
+  count += 1
+  stopInterval(count)
+  console.log('Hello Fresher Academy')
+}, 3000)
+function stopInterval (count) {
+  if (count > 2) {
+    clearInterval(abc)
+  }
+}
 
 // Exercise 03: Event Loop
 // function abc() {
@@ -32,76 +32,76 @@
 
 // Exercise 04: Event Loop cont
 // Case 1
-console.log("A");
-setImmediate(function() { console.log("B"); }, 0);
-setImmediate(function() { console.log("C"); }, 0)
-console.log("D");
-//-> ADBC
+// console.log("A");
+// setImmediate(function() { console.log("B"); }, 0);
+// setImmediate(function() { console.log("C"); }, 0)
+// console.log("D");
+// //-> ADBC
 
-// Case 2
-setTimeout(function() {
-  setTimeout(function() {
-    console.log('A');
-  }, 0);
-}, 0);
+// // Case 2
+// setTimeout(function() {
+//   setTimeout(function() {
+//     console.log('A');
+//   }, 0);
+// }, 0);
 
-setTimeout(function() {
-  console.log('B');
-}, 0);
+// setTimeout(function() {
+//   console.log('B');
+// }, 0);
 
-setTimeout(function() {
-  setTimeout(function() {
-    console.log('C');
-  }, 0);
-}, 10);
+// setTimeout(function() {
+//   setTimeout(function() {
+//     console.log('C');
+//   }, 0);
+// }, 10);
 
-setTimeout(function() {
-  console.log('D');
-}, 0);
-//->BDAC
+// setTimeout(function() {
+//   console.log('D');
+// }, 0);
+// //->BDAC
 
-// Case 3
-var x = 'A';
+// // Case 3
+// var x = 'A';
 
-setTimeout(function() {
-  console.log(x);
-  x = 'B';
-}, 3);
+// setTimeout(function() {
+//   console.log(x);
+//   x = 'B';
+// }, 3);
 
-setTimeout(function() {
-  console.log(x);
-  x = 'C';
-}, 2);
+// setTimeout(function() {
+//   console.log(x);
+//   x = 'C';
+// }, 2);
 
-setTimeout(function() {
-  console.log(x);
-  x = 'D';
-}, 1);
+// setTimeout(function() {
+//   console.log(x);
+//   x = 'D';
+// }, 1);
 
-setTimeout(function() {
-  console.log(x);
-}, 4);
-//->ABCD
+// setTimeout(function() {
+//   console.log(x);
+// }, 4);
+// //->ABCD
 
-// Case 4
-var t1 = setTimeout(function() {
-  console.log('A');
-  setTimeout(function() {
-    console.log('B');
-  }, 0);
-}, 100);
+// // Case 4
+// var t1 = setTimeout(function() {
+//   console.log('A');
+//   setTimeout(function() {
+//     console.log('B');
+//   }, 0);
+// }, 100);
 
-var t2 = setTimeout(function() {
-  console.log('C');
-  setTimeout(function() {
-    console.log('D');
-  }, 0);
-}, 200);
+// var t2 = setTimeout(function() {
+//   console.log('C');
+//   setTimeout(function() {
+//     console.log('D');
+//   }, 0);
+// }, 200);
 
-clearTimeout(t1);
+// clearTimeout(t1);
 
-setTimeout(function() {
-  clearTimeout(t2);
-}, 250);
-//->ACBD
+// setTimeout(function() {
+//   clearTimeout(t2);
+// }, 250);
+// //->ACBD
 
